310f515b1003ca750846b28bfd3588a52bcc7e7d
Denis Serkhel <ripvanfish27@gmail.com>
2022-07-12 00:17:25 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header'
 import './App.css';
+import HeroSection from './components/HeroSection/HeroSection';
 
 function App() {
   return (
     <div className='container'>
       <div className='hero_wrap'>
         <Header />
+        <HeroSection />
         </div>
       
     </div>
---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,12 @@
+import css from './HeroSection.module.css'
+
+
+const HeroSection = () => {
+    return (
+        <div className={css.wrap}>
+
+        </div>
+    )
+}
+
+export default HeroSection;
